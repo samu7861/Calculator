577a97fb446de5e5bfa733e0c3cd387d3e7999f0
samu7861 <sfrancog7861@gmail.com>
2023-04-11 18:29:10 -0500

    logica inicial

--- src/script.js
+++ src/script.js
@@ -1,2 +1,31 @@
-const pantalla = document.querySelector(".pantalla")
+const pantalla = document.querySelector(".Pantalla")
 const botones = document.querySelectorAll(".btn")
+
+
+//leer botones
+botones.forEach(boton => {
+    boton.addEventListener("click", ()=> {
+        const botonApretado = boton.textContent;
+
+        //función C
+        if (boton.id === "c"){
+            pantalla.textContent = "0"
+            return;
+        }
+
+        //función delete
+        if (boton.id === "delete"){
+            pantalla.textContent[-1]=null
+            return;
+        }
+
+
+        //escribir números.
+        if(pantalla.textContent === "0"){
+            pantalla.textContent=botonApretado
+        }else{
+            pantalla.textContent += botonApretado;
+        }
+
+    })
+})
