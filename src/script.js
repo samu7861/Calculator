bd6c9598e825f369fa862ad59c7c4fae09bbb3c1
samu7861 <sfrancog7861@gmail.com>
2023-04-11 18:47:31 -0500

    final code

--- src/script.js
+++ src/script.js
@@ -15,13 +15,28 @@ botones.forEach(boton => {
 
         //función delete
         if (boton.id === "delete"){
-            pantalla.textContent[-1]=null
+            if (pantalla.textContent.length === 1 || pantalla.textContent ==="Error"){
+                pantalla.textContent = "0"
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0, -1)
+            }
+            return;
+        }
+
+        //función cálcular
+        if(boton.id === "igual"){
+            try{
+                pantalla.textContent=eval(pantalla.textContent)
+            }catch{
+                pantalla.textContent="Error"
+            }
+            
             return;
         }
 
 
         //escribir números.
-        if(pantalla.textContent === "0"){
+        if(pantalla.textContent === "0" || pantalla.textContent==="Error"){
             pantalla.textContent=botonApretado
         }else{
             pantalla.textContent += botonApretado;
